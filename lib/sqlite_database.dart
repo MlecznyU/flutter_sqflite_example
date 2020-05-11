1506ce2b3953d95e06e4d1976ef4840b64adf8fd
Tomasz Michlewski <Tomasz.Michlewski@gmail.com>
2020-05-11 17:34:35 +0200

    table names as consts

--- lib/sqlite_database.dart
+++ lib/sqlite_database.dart
@@ -1,6 +1,12 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+const idColumnName = 'id';
+const titleColumnName = 'title';
+const isDoneColumnName = 'is_done';
+const tableName = 'tasks';
+const databaseName = 'database.db';
+
 class TaskModel {
   final int id;
   final String title;
@@ -10,17 +16,17 @@ class TaskModel {
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
-      'title': title,
-      'isDone': isDone == true ? 1 : 0,
+      idColumnName: id,
+      titleColumnName: title,
+      isDoneColumnName: isDone == true ? 1 : 0,
     };
   }
 
   static TaskModel fromMap(final Map<String, dynamic> map) {
     return TaskModel(
-      map['id'],
-      map['title'],
-      map['isDone'] == 0 ? false : true,
+      map[idColumnName],
+      map[titleColumnName],
+      map[isDoneColumnName] == 0 ? false : true,
     );
   }
 }
@@ -29,59 +35,56 @@ class SqliteDatabase {
   Future<Database> getDatabase() async {
     return openDatabase(
       await getSqliteDatabasePath(),
-      onCreate: onSqliteDatabaseCreate,
+      onCreate: onDatabaseCreate,
       version: 1,
     );
   }
 
-  Future<void> onSqliteDatabaseCreate(
+  Future<void> onDatabaseCreate(
     final Database database,
     final int version,
   ) {
     return database.execute(
-        'CREATE TABLE tasks(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT, isDone INTEGER)');
+        'CREATE TABLE $tableName($idColumnName INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, $titleColumnName TEXT, $isDoneColumnName INTEGER)');
   }
 
   Future<String> getSqliteDatabasePath() async {
-    return join(await getDatabasesPath(), 'database.db');
+    return join(await getDatabasesPath(), databaseName);
   }
 
   Future<List<TaskModel>> getAllTasks() async {
     final Database db = await getDatabase();
-    final List<Map<String, dynamic>> result = await db.query(
-      'tasks', //name of the table
-    );
+    final List<Map<String, dynamic>> result = await db.query(tableName);
     return result.map(TaskModel.fromMap).toList();
   }
 
   Future<void> insertNewTask(final TaskModel newTask) async {
     final Database db = await getDatabase();
     await db.insert(
-      'tasks',
+      tableName,
       newTask.toMap(),
       conflictAlgorithm: ConflictAlgorithm.fail,
-      // definition of how the base should behave if we try
-      // to insert an object with the same primary key in it
-      // fail - throws an exception
+      // conflictAlgorithm definition of how the base should
+      // behave if we try to insert an object with the same
+      // primary key. Fail - throws an exception
     );
   }
 
   Future<void> updateTask(final TaskModel updatedTask) async {
     final Database db = await getDatabase();
     await db.update(
-      'tasks',
+      tableName,
       updatedTask.toMap(),
-      where: 'id = ?',
+      where: '$idColumnName = ?',
       whereArgs: [updatedTask.id],
     );
   }
 
   Future<void> deleteTask(final int taskId) async {
     final Database db = await getDatabase();
-
     await db.delete(
-      'tasks',
-      where: 'id = ?',
+      tableName,
+      where: '$idColumnName = ?',
       whereArgs: [taskId],
     );
   }
